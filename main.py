ccc34953b05bbf7cd94cb9ee24e027277bae4e6d
Josafa Dieb <josafadieb@gmail.com>
2023-07-03 22:55:13 -0300

    Implementação sem modelo

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import dpll
+from pysat.solvers import Glucose4
 
+# Normalization
+file=open("./templates/5-rainhas.txt", 'r')
+lines=[line.replace("\n", "").replace("\r", "").split(" ") for line in file.readlines()]
 
-cnf = [[1, 2, -3], [1, -2, 3], [1, -2, -3], [-1, 2, 3], [-1, -2, 3], [-1, 2, -3], [-1, -2, -3]]
-
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        lines[i][j] = int(lines[i][j])
+        if i >= 1:
+            if lines[i][j] < 0:
+                print("Q", end=" ")
+            else:
+                print("#", end=" ")
+    print("")
+cnf = lines[1:]
 for clause in cnf:
     dpll.add_clause(clause)
 
